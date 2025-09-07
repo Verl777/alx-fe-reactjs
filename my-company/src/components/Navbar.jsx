5a0645031ec4e38f341d0a8e7866593de094942e
Verl777 <morahvalerianne@gmail.com>
2025-09-07 20:51:45 +0300

    Alx react app My company

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,12 +1,21 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: '#222', padding: '10px', textAlign: 'center' }}>
-      <Link to="/" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>Home</Link>
-      <Link to="/about" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>About</Link>
-      <Link to="/services" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>Services</Link>
-      <Link to="/contact" style={{ color: 'white', margin: '0 15px', textDecoration: 'none' }}>Contact</Link>
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-around",
+        alignItems: "center",
+        padding: "10px",
+        backgroundColor: "#333",
+        color: "#fff"
+      }}
+    >
+      <Link to="/" style={{ color: "#fff", textDecoration: "none" }}>Home</Link>
+      <Link to="/about" style={{ color: "#fff", textDecoration: "none" }}>About</Link>
+      <Link to="/services" style={{ color: "#fff", textDecoration: "none" }}>Services</Link>
+      <Link to="/contact" style={{ color: "#fff", textDecoration: "none" }}>Contact</Link>
     </nav>
   );
 }
