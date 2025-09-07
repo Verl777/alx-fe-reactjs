dd9dee79c29ee40be96175995df17846d8b5929d
Verl777 <morahvalerianne@gmail.com>
2025-09-07 19:51:31 +0300

    Alx react app new

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './components/Header'
 import MainContent from './components/MainContent'
 import Footer from './components/Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -46,6 +47,10 @@ function App() {
                 bio="A passionate software developer" 
             />
         </div>
+         <div>
+      <h1 style={{ textAlign: 'center' }}>React Counter App</h1>
+      <Counter />
+    </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
